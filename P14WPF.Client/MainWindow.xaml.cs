a34c3fde2c4c0523395ca9ccf2f39d9d1e569580
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-10-30 12:01:24 +0100

    q

--- P14WPF.Client/MainWindow.xaml.cs
+++ P14WPF.Client/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using P12Weather.Shared;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Reactive.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +21,64 @@ namespace P14WPF.Client
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private IObservable<WeatherForecast> _weatherForecast;
+
+        private ObservableCollection<WeatherForecast> _weatherForecasts = new ObservableCollection<WeatherForecast>();
         public MainWindow()
         {
             InitializeComponent();
+
+            WeatherListBox.ItemsSource = _weatherForecasts;
+
+            _weatherForecast = GetWeatherUpdates("http://localhost:5043/weatherforecast");
+            StartWeatherListener();
+        }
+
+        private void StartWeatherListener()
+        {
+            _weatherForecast
+                .Subscribe(
+                    x => Dispatcher.Invoke(()=> _weatherForecasts.Add(x)),
+                    error => MessageBox.Show(error.Message));
+        }
+
+        private IObservable<WeatherForecast> GetWeatherUpdates(string apiUrl)
+        {
+            return Observable.Create<WeatherForecast>(async (observer, ct) =>
+             {
+                 try
+                 {
+                     using(var client = new HttpClient())
+                     {
+                         while (true) 
+                         {
+                             var response = await client.GetAsync(apiUrl);
+                             if (response.IsSuccessStatusCode)
+                             {
+                                 var content = await response.Content.ReadFromJsonAsync<WeatherForecast[]>();
+
+                                 foreach (var item in content)
+                                 {
+                                     observer.OnNext(item);
+                                 }
+                             }
+                             else
+                             {
+                                 observer.OnError(new Exception("Api error"));
+                             }
+
+                             await Task.Delay(5000);
+                         }
+                     }
+
+
+                 }
+                 catch (Exception ex)
+                 {
+                     observer.OnError(ex);
+                 }
+             });
         }
     }
 }
