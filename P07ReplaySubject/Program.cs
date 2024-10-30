8436e639cd58d4b17ab157a877a429973f177b49
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-10-30 10:00:34 +0100

    q

---
+++ P07ReplaySubject/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Reactive.Subjects;
+
+namespace Example1
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            var market = new Subject<float>(); 
+
+            market.OnNext(1);
+
+            market.Subscribe(x => Console.WriteLine($"Observer 1: {x}"));
+        }
+    }
+}
+
+namespace Example2
+{
+    internal class Program
+    {
+        static void Main2(string[] args)
+        {
+            var market = new ReplaySubject<float>(2);
+
+            market.OnNext(1);
+           
+            market.OnNext(2);
+
+            market.OnNext(3);
+
+            market.Subscribe(x => Console.WriteLine($"Observer 1: {x}"));
+
+        }
+    }
+}
+
+namespace Example3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var timeWinodw = TimeSpan.FromMilliseconds(500);
+            var market = new ReplaySubject<float>(timeWinodw);
+
+            market.OnNext(1);
+            Thread.Sleep(200);
+            market.OnNext(2);
+            Thread.Sleep(200);
+            market.OnNext(3);
+            Thread.Sleep(200);
+
+            market.Subscribe(x => Console.WriteLine($"Observer 1: {x}"));
+
+            market.OnNext(4);
+
+        }
+    }
+}
---
+++ P08BehaviorSubject/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Reactive.Subjects;
+
+namespace Example1
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+           
+            var sensor = new BehaviorSubject<float>(1.0f);
+
+
+            sensor.Subscribe(Console.WriteLine);
+
+            sensor.OnNext(2.0f);
+
+            var otherObserver = sensor.Subscribe(
+                x=> Console.WriteLine($"other observer generated value {x}"),
+                ex => Console.WriteLine($"error : {ex}"));
+        }
+    }
+}
+
+namespace Example2
+{
+    public class Scada
+    {
+
+        private BehaviorSubject<int> sensorValue;
+
+        public Scada(int initialValue)
+        {
+            sensorValue = new BehaviorSubject<int>(initialValue);
+        }
+
+        public IObservable<int> SensorValue => sensorValue;
+
+        internal void UpdateSensorValue(int v)
+        {
+            sensorValue.OnNext(v);
+        }
+
+        internal void CompleteSensor()
+        {
+            sensorValue.OnCompleted();
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var scadaSystem = new Scada(1);
+
+            scadaSystem.SensorValue.Subscribe(Console.WriteLine);
+
+            scadaSystem.UpdateSensorValue(2);
+
+            scadaSystem.CompleteSensor();
+        }
+    }
+}
