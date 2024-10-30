543a340c1ca1f0fa0e3741c54a4f90c9d904740c
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-10-30 11:32:47 +0100

    q

--- P13Weather.Test/UnitTest1.cs
+++ P13Weather.Test/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace P13Weather.Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GetWeatherForeast_REturnsCorrentData_WhenApiREturnsValidData()
         {
             var mockHttp = new Mock<HttpClient>(); 
             var weatherService = new Mock<WeatherService>(mockHttp.Object);
@@ -20,9 +20,24 @@ namespace P13Weather.Test
             weatherService.Object.GetWeatherForecast("http://localhost:5043/weatherforecast").Subscribe(
                 x => Assert.Equal(testWeatherData, x),
                 error => Assert.True(false, error.Message));
+        }
+
+        [Fact]
+        public void GetWeatherForeast_Error_WhenApiFails()
+        {
+            var mockHttp = new Mock<HttpClient>();
+            var weatherService = new Mock<WeatherService>(mockHttp.Object);
+
+            string testWeatherData = "0";
+
+            weatherService.Setup(x => x.GetWeatherForecast(It.IsAny<string>()))
+                .Returns(Observable.Throw<string>(new Exception("Api error"))
+                .Catch(Observable.Return("0")));
 
 
-
+            weatherService.Object.GetWeatherForecast("http://localhost:5043/weatherforecast").Subscribe(
+                x => Assert.Equal(testWeatherData, x),
+                error => Assert.True(false, error.Message));
         }
     }
 }
