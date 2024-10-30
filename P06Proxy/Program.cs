c5729d7a84c8608fe0081f7c2b2658fb3e80bbd8
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-10-30 09:20:50 +0100

    q

--- P06Proxy/Program.cs
+++ P06Proxy/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Runtime.CompilerServices;
 using System.Threading.Channels;
 
-namespace P06Proxy
+namespace Example1
 {
     public static class ExtensionMethod
     {
@@ -15,7 +16,7 @@ namespace P06Proxy
 
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
            
             var market = new Subject<float>(); //  Rynek , który jest obserwowany przez inwestorów
@@ -28,7 +29,10 @@ namespace P06Proxy
                 .Subscribe(marketConsumer); // subskrypcja rynku do inwestorów
 
 
-            marketConsumer.Subscribe(Console.WriteLine); // wyswietlenie wartosci 
+           // marketConsumer.Subscribe(Console.WriteLine); // wyswietlenie wartosci 
+           var anotherObserver = new AnotherObserver();
+            marketConsumer.Subscribe(anotherObserver);
+
 
             market.OnNext(1);
             market.OnNext(2);
@@ -37,4 +41,97 @@ namespace P06Proxy
             market.OnCompleted();
         }
     }
+    public class AnotherObserver : IObserver<float>
+    {
+        public void OnCompleted()
+        {
+            Console.WriteLine("Another observer completed");
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("Another observer error");
+        }
+
+        public void OnNext(float value)
+        {
+            Console.WriteLine($"Another observer value: {value}");
+        }
+    }
+
+
+
+}
+
+
+namespace Example2
+{
+    public static class ExtensionMethod
+    {
+     public static IDisposable Inspect<T>(this IObservable<T> self, string name)
+        {
+            return self.Subscribe(
+                value => Console.WriteLine($"{name} - OnNext({value})"),
+                error => Console.WriteLine($"{name} - OnError({error.Message})"),
+                () => Console.WriteLine($"{name} - OnCompleted()"));
+        }  
+
+        public static IObserver<T> OnNext<T>(this IObserver<T> self, params T[] args)
+        {
+            foreach (var arg in args)
+            {
+                self.OnNext(arg);
+            }
+            return self;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var market = new Subject<float>(); //  Rynek , który jest obserwowany przez inwestorów
+            var marketConsumer = new Subject<float>(); // Inwestorzy, którzy obserwują rynek
+
+         //   marketConsumer.SubscribeTo(market); // subskrypcja inwestorów do rynku
+
+            market.Subscribe(marketConsumer); // subskrypcja rynku do inwestorów
+
+
+            // marketConsumer.Subscribe(Console.WriteLine); // wyswietlenie wartosci 
+            //var anotherObserver = new AnotherObserver();
+           // marketConsumer.Subscribe(anotherObserver);
+           marketConsumer.Inspect("MarketConsumer");
+
+
+            //market.OnNext(1);
+            //market.OnNext(2);
+            //market.OnNext(3);
+            //market.OnNext(4);
+            market.OnNext(1, 2, 3, 4);
+            market.OnCompleted();
+        }
+    }
+    public class AnotherObserver : IObserver<float>
+    {
+        public void OnCompleted()
+        {
+            Console.WriteLine("Another observer completed");
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("Another observer error");
+        }
+
+        public void OnNext(float value)
+        {
+            Console.WriteLine($"Another observer value: {value}");
+        }
+    }
+
+
+
 }
