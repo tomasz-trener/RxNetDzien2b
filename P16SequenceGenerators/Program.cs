f69136d07469fda29d5d46a47a28e41e57df3214
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-10-30 13:46:34 +0100

    q

---
+++ P16SequenceGenerators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reactive.Linq;
+
+namespace P16SequenceGenerators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var values = Observable.Range(10, 20);
+
+          //  values.Subscribe(x => Console.WriteLine(x));
+
+
+            var values2 = Observable.Generate(
+                1,
+                x => x < 100,
+                x => x * 2,
+                x => $"[{x}]"
+                );
+
+            values2.Subscribe(x => Console.WriteLine(x));
+
+
+            var timer = Observable.Timer(TimeSpan.FromSeconds(2));
+
+            timer.Subscribe(x => Console.WriteLine($"Timer {x}"));
+        }
+    }
+}
---
+++ P17ObservableCreate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reactive.Linq;
+
+namespace P17ObservableCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = Observable.Create<string>(observer =>
+            {
+                var timer = new System.Timers.Timer(1000);
+                timer.Elapsed += (sender, e) =>
+                {
+                    observer.OnNext($"Tick {e.SignalTime}");
+                };
+
+                timer.Start();
+                return () =>
+                {
+                    timer.Stop();
+                    timer.Dispose();
+                };
+            });
+
+
+            var subscription = o.Subscribe(x => Console.WriteLine(x));
+
+            Console.ReadKey();
+
+        }
+    }
+}
