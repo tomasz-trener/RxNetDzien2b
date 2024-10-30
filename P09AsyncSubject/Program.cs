68626b3a15fa6870a65aa1797fac0a4f42465de3
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-10-30 10:44:49 +0100

    q

---
+++ P09AsyncSubject/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Reactive.Subjects;
+
+namespace Example1
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            AsyncSubject<double> subject = new AsyncSubject<double>();
+
+            subject.Subscribe(value => System.Console.WriteLine($"Observer 1: {value}"));
+
+            subject.OnNext(1.0);
+            subject.OnNext(2.0);
+
+            subject.OnCompleted();
+
+        }
+    }
+}
+
+namespace Example2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AsyncSubject<double> subject = new AsyncSubject<double>();
+
+            subject.Subscribe(value => System.Console.WriteLine($"Observer 1: {value}"));
+
+
+            //   Task.Run(() => SimulateAsyncOperation(subject));
+            await SimulateAsyncOperation(subject);
+
+            Console.WriteLine("Finish process");
+            Console.ReadKey();
+
+        }
+
+        static async Task SimulateAsyncOperation(AsyncSubject<double> subject)
+        {
+            Console.WriteLine("Start operation..");
+            await Task.Delay(1000);
+            subject.OnNext(1.0);
+            await Task.Delay(1000);
+            subject.OnNext(2.0);
+            subject.OnCompleted();
+        }
+    }
+}
---
+++ P10DequenceEmission/Program.cs
@@ -0,0 +1,59 @@
+﻿using P12Weather.Shared;
+using System.Reactive.Linq;
+using System.Text.Json;
+
+namespace P10DequenceEmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var obs=  Observable.Return(42);
+
+            //obs.Subscribe(x=> Console.WriteLine($"Completed {x}"));
+
+            var weatherService = new WeatherService(new HttpClient());
+            string apiUrl = "http://localhost:5043/weatherforecast";
+
+            var weatherForecast = weatherService.GetWeatherForecast(apiUrl);
+
+
+            weatherForecast.Subscribe(
+                x => Console.WriteLine($"Data from forecast: {x}"),
+                error => Console.WriteLine($"Error fetching data {error.Message}"));
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+
+
+    public class WeatherService
+    {
+        private HttpClient _httpClient;
+
+        public WeatherService(HttpClient client)
+        {
+            _httpClient = client;
+        }
+
+        public IObservable<string> GetWeatherForecast(string apiUrl)
+        {
+            var o = Observable.FromAsync(async ct => await GetWeatherForecastAsync(apiUrl));
+            return o;
+
+        }
+
+        public async Task<string> GetWeatherForecastAsync(string apiUrl)
+        {
+            var response = await _httpClient.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+
+            string content = await response.Content.ReadAsStringAsync();
+            var weatherForecast = JsonSerializer.Deserialize<WeatherForecast[]>(content);
+
+            return string.Join("\n", weatherForecast.Select(x => $"Date: {x.Date}, Temp: {x.TemperatureC}, Summary {x.Summary}"));
+
+        }
+    }
+}
